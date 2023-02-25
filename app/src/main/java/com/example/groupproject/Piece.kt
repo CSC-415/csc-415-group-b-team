335fcb3705b429bef7471af14dd1637442c1d1c1
chalkcoral <73558535+chalkcoral@users.noreply.github.com>
2023-02-25 18:59:58 -0500

    started implementation of abstract peices

--- app/src/main/java/com/example/groupproject/Piece.kt
+++ app/src/main/java/com/example/groupproject/Piece.kt
@@ -1,6 +1,6 @@
 package com.example.groupproject
 
-abstract class Piece(x: Int, y: Int, name: String, board: Board) {
+abstract class Piece(val x: Int,val y: Int,val name: String,val board: Board) {
 
     init {
         var x : Int = x
@@ -13,4 +13,32 @@ abstract class Piece(x: Int, y: Int, name: String, board: Board) {
             isWhitePiece = true
         }
     }
+}
+class Pawn(x: Int, y: Int, name: String, board: Board) : Piece(x, y, name, board) {
+    init {
+        var x : Int = x
+        var y : Int = y
+        val name : String = "pawn"
+        var board : Board
+        var isWhitePiece : Boolean = false
+        var moveSet : MutableList<String> = mutableListOf()
+        if (name.first() == 'w') {
+            isWhitePiece = true
+        }
+    }
+}
+class Rook(x: Int, y: Int, name: String, board: Board) : Piece(x, y, name, board) {
+
+}
+class King(x: Int, y: Int, name: String, board: Board) : Piece(x, y, name, board) {
+
+}
+class Queen(x: Int, y: Int, name: String, board: Board) : Piece(x, y, name, board) {
+
+}
+class Bishop(x: Int, y: Int, name: String, board: Board) : Piece(x, y, name, board) {
+
+}
+class Knight(x: Int, y: Int, name: String, board: Board) : Piece(x, y, name, board) {
+
 }
