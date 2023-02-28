3c1ecca52796b83a09a7443a7eae3620ad14cc51
Fitzpatrit2 <117688467+Fitzpatrit2@users.noreply.github.com>
2023-02-28 17:56:36 -0500

    Add files via upload

---
+++ PieceReference.kt
@@ -0,0 +1,10 @@
+package com.example.groupproject
+
+data class PieceReference(
+    val Rook: String,
+    val Knight: String,
+    val Bishop: String,
+    val King: String,
+    val Queen: String,
+    val Pawn: String,
+)
