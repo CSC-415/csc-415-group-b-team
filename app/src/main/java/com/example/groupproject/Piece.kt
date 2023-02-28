9b5d8253f2e3ab7bdae6a106c52071dc9eb7f60d
Tristan Fitzpatrick <tristanfitzpatrick@outlook.com>
2023-02-28 16:46:51 -0500

    first commit

---
+++ app/src/main/java/com/example/groupproject/Piece.kt
@@ -0,0 +1,16 @@
+package com.example.groupproject
+
+abstract class Piece(x: Int, y: Int, name: String, board: Board) {
+
+    init {
+        var x : Int = x
+        var y : Int = y
+        val name : String = name
+        var board : Board
+        var isWhitePiece : Boolean = false
+        var moveSet : MutableList<String> = mutableListOf()
+        if (name.first() == 'w') {
+            isWhitePiece = true
+        }
+    }
+}
---
+++ app/src/main/java/com/example/groupproject/Space.kt
@@ -0,0 +1,33 @@
+package com.example.groupproject
+
+import java.lang.reflect.Constructor
+
+class Space {
+    var isBlackSpace : Boolean = false
+
+    constructor(x : Int, y : Int) {
+        val x = x
+        val y = y
+        val piece = null
+
+        if (x % 2 != 0 && y % 2 == 0) {
+            isBlackSpace = true
+        }
+        if (x % 2 == 0 && y % 2 != 0) {
+            isBlackSpace = true
+        }
+    }
+
+    constructor(x : Int, y : Int, piece : Piece?) {
+        val x = x
+        val y = y
+        val piece = piece
+
+        if (x % 2 != 0 && y % 2 == 0) {
+            isBlackSpace = true
+        }
+        if (x % 2 == 0 && y % 2 != 0) {
+            isBlackSpace = true
+        }
+    }
+}
