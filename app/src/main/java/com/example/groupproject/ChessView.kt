757f3a5df75d96f3043d1709e8f63296d911159c
Tyler <tylermmonroe@outlook.com>
2023-04-10 20:07:57 -0400

    Pawn & Rook validation implemented

--- app/src/main/java/com/example/groupproject/ChessView.kt
+++ app/src/main/java/com/example/groupproject/ChessView.kt
@@ -70,7 +70,7 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
         if (piece.isWhitePiece) { //white piece logic
             if (piece.x == 6) { //starting move
                 if (y == piece.y) { //non-capture move
-                    if (x - piece.x == 1 || x - piece.x == 2) { //move forward 1 or 2 spaces
+                    if (piece.x - x == 1 || piece.x - x == 2) { //move forward 1 or 2 spaces
                         if (checkCollision(x,y) == null) { //nothing in the way
                             return Validation(true, false)
                         } else { //invalid move, something in the way
@@ -80,20 +80,88 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
                         return Validation(false, false)
                     }
                 } else { //capture move
-                    if (checkCollision(x,y) == null) { //nothing in the way
+                    val targetPiece = checkCollision(x,y)
+                    if (targetPiece == null) { //nothing in the way
                         return Validation(true, false)
                     } else { //invalid move, something in the way
-                        
+                        if (!targetPiece.isWhitePiece) { //targetPiece is not on same side as piece, valid move w/ capture
+                            return Validation(true,true)
+                        } else { //targetPiece is on same side as piece, invalid move
+                            return Validation(false,false)
+                        }
                     }
                 }
             } else { //non starting move
-
+                if (y == piece.y) { //non-capture move
+                    if (piece.x - x == 1) { //move forward 1 or 2 spaces
+                        if (checkCollision(x,y) == null) { //nothing in the way
+                            return Validation(true, false)
+                        } else { //invalid move, something in the way
+                            return Validation(false, false)
+                        }
+                    } else { //invalid move
+                        return Validation(false, false)
+                    }
+                } else { //capture move
+                    val targetPiece = checkCollision(x,y)
+                    if (targetPiece == null) { //nothing in the way
+                        return Validation(true, false)
+                    } else { //invalid move, something in the way
+                        if (!targetPiece.isWhitePiece) { //targetPiece is not on same side as piece, valid move w/ capture
+                            return Validation(true,true)
+                        } else { //targetPiece is on same side as piece, invalid move
+                            return Validation(false,false)
+                        }
+                    }
+                }
             }
         } else { //black piece logic
             if (piece.x == 1) { //starting move
-
+                if (y == piece.y) { //non-capture move
+                    if (x -piece.x == 1 || x - piece.x == 2) { //move forward 1 or 2 spaces
+                        if (checkCollision(x,y) == null) { //nothing in the way
+                            return Validation(true, false)
+                        } else { //invalid move, something in the way
+                            return Validation(false, false)
+                        }
+                    } else { //invalid move
+                        return Validation(false, false)
+                    }
+                } else { //capture move
+                    val targetPiece = checkCollision(x,y)
+                    if (targetPiece == null) { //nothing in the way
+                        return Validation(true, false)
+                    } else { //invalid move, something in the way
+                        if (targetPiece.isWhitePiece) { //targetPiece is not on same side as piece, valid move w/ capture
+                            return Validation(true,true)
+                        } else { //targetPiece is on same side as piece, invalid move
+                            return Validation(false,false)
+                        }
+                    }
+                }
             } else { //non starting move
-
+                if (y == piece.y) { //non-capture move
+                    if (x - piece.x == 1) { //move forward 1 or 2 spaces
+                        if (checkCollision(x,y) == null) { //nothing in the way
+                            return Validation(true, false)
+                        } else { //invalid move, something in the way
+                            return Validation(false, false)
+                        }
+                    } else { //invalid move
+                        return Validation(false, false)
+                    }
+                } else { //capture move
+                    val targetPiece = checkCollision(x,y)
+                    if (targetPiece == null) { //nothing in the way
+                        return Validation(true, false)
+                    } else { //invalid move, something in the way
+                        if (targetPiece.isWhitePiece) { //targetPiece is not on same side as piece, valid move w/ capture
+                            return Validation(true,true)
+                        } else { //targetPiece is on same side as piece, invalid move
+                            return Validation(false,false)
+                        }
+                    }
+                }
             }
         }
     }
@@ -244,7 +312,121 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
         }
         return Validation(false, false)
     }
-    private fun validateRook (piece: Piece, x: Int, y: Int) : Validation {}
+    private fun validateRook (piece: Piece, x: Int, y: Int) : Validation {
+        val hitPiece = false
+        if (piece.isWhitePiece == true) { //white piece logic
+            if (y == piece.y) { //rook is moving up/down
+                if (x > piece.x) { //rook is moving towards white start
+                    for (i in piece.x..x) {
+                        val targetPiece = checkCollision(i, y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.x == x && targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.x == x && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                    return Validation(true, false) //nothing in the way, move is valid w/o capture
+                } else  { //rook is moving towards black start
+                    for (i in piece.x downTo x) {
+                        val targetPiece = checkCollision(i, y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.x == x && targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.x == x && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                    return Validation(true, false) //nothing in the way, move is valid w/o capture
+                }
+            } else if (x == piece.x) { //rook is moving left/right
+                if (y > piece.y) { //rook is moving to the right
+                    for (i in piece.y..y) {
+                        val targetPiece = checkCollision(x, y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.y == y && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.y == y && targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                    return Validation(true, false) //nothing in the way, move is valid w/o capture
+                } else { //rook is moving to the left
+                    for (i in piece.y downTo y) {
+                        val targetPiece = checkCollision(x, y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.y == y && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.y == y && targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                    return Validation(true, false) //nothing in the way, move is valid w/o capture
+                }
+            } else { //invalid move, rook is trying to move diagonal
+                return Validation(false,false)
+            }
+        } else { //black piece logic
+            if (y == piece.y) { //rook is moving up/down
+                if (x > piece.x) { //rook is moving towards white start
+                    for (i in piece.x..x) {
+                        val targetPiece = checkCollision(i,y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.x == x && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.x == x && targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                    return Validation(true, false) //nothing in the way, move is valid w/o capture
+                } else  { //rook is moving towards black start
+                    for (i in piece.x downTo x) {
+                        val targetPiece = checkCollision(i,y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.x == x && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.x == x && targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                    return Validation(true, false) //nothing in the way, move is valid w/o capture
+                }
+            } else if (x == piece.x) { //rook is moving left/right
+                if (y > piece.y) { //rook is moving to the right
+                    for (i in piece.y..y) {
+                        val targetPiece = checkCollision(x, y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.y == y && targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.y == y && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                } else { //rook is moving to the left
+                    for (i in piece.y downTo y) {
+                        val targetPiece = checkCollision(x, y)
+                        if (targetPiece != null) { //piece was found
+                            if (targetPiece.y == y && targetPiece.isWhitePiece) { //piece is where rook is going to and is on opposing side, valid move w/ capture
+                                return Validation(true,true)
+                            } else if (targetPiece.y == y && !targetPiece.isWhitePiece) { //piece is where rook is going to and is on same side, invalid move
+                                return Validation(false,false)
+                            }
+                        }
+                    }
+                }
+            } else { //invalid move, rook is trying to move diagonal
+                return Validation(false,false)
+            }
+        }
+        return Validation(false, false)
+    }
     private fun validateQueen (piece: Piece, x: Int, y: Int) : Validation {}
     private fun validateKing (piece: Piece, x: Int, y: Int) : Validation {
         if((x-1==piece.x || x+1==piece.x) && (y-1==piece.y || y+1==piece.y) ) {  //if move want to do is within 1 space of original king
