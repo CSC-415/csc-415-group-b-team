e3446309761bd4f4ccdfcdb79273507f04522786
Mo71999 <65233007+Mo71999@users.noreply.github.com>
2023-04-20 16:41:27 -0400

    Add files via upload

--- app/src/main/java/com/example/groupproject/MainActivity.kt
+++ app/src/main/java/com/example/groupproject/MainActivity.kt
@@ -1,26 +1,84 @@
 package com.example.groupproject
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.groupproject.data.Pieces
-import com.example.groupproject.data.Space
-const val TAG = "MainActivity"
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.text.format.Time
+import com.example.groupproject.databinding.ClockTimerBinding
+import kotlin.math.roundToInt
 
-class MainActivity : AppCompatActivity(), Board {
-    var chessModel = ChessModel()
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ClockTimerBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ClockTimerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<ChessView>(R.id.chess_view).board = this
+        binding.startStopButton.setOnClickListener { startStopTimer() }
+        binding.resetButton.setOnClickListener { resetTimer() }
+
+        serviceIntent = Intent(applicationContext, Timer::class.java)
+        registerReceiver(updateTime, IntentFilter(Timer.TIMER_UPDATED))
+    }
+    private fun resetTimer()
+    {
+        stopTimer()
+        time = 0.0
+        binding.timeTV.text = getTimeStringFromDouble(time)
     }
 
-    override fun pieceAt(col: Int, row: Int): Pieces? {
-        return chessModel.pieceAt(col, row)
+    private fun startStopTimer()
+    {
+        if(timerStarted)
+            stopTimer()
+        else
+            startTimer()
     }
 
-    override fun movePiece(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int) {
-        chessModel.movePiece(fromCol, fromRow, toCol, toRow)
-        findViewById<ChessView>(R.id.chess_view).invalidate()
+    private fun startTimer()
+    {
+        serviceIntent.putExtra(Timer.TIME_EXTRA, time)
+        startService(serviceIntent)
+        binding.startStopButton.text = "Stop"
+        binding.startStopButton.icon = getDrawable(R.drawable.ic_baseline_pause_24)
+        timerStarted = true
     }
+
+    private fun stopTimer()
+    {
+        stopService(serviceIntent)
+        binding.startStopButton.text = "Start"
+        binding.startStopButton.icon = getDrawable(R.drawable.ic_baseline_play_24)
+        timerStarted = false
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onReceive(context: Context, intent: Intent)
+        {
+            time = intent.getDoubleExtra(Timer.TIME_EXTRA, 0.0)
+            binding.timeTV.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String
+    {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d", hour, min, sec)
 }
